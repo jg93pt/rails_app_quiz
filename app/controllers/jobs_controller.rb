c5c657c6ac906d4aa1a6c208db4d5b379fb96334
jg93pt <jorgebenf@gmail.com>
2022-05-13 14:56:03 +0100

    Created view create.jbuilder for POST and created GET all Jobs Method

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,15 @@
 class JobsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  #Create Jobs - POST
+  #POST /jobs
   def create
-    new_job = Job.create(url: params[:url], employer_name: params[:employer_name], employer_description: params[:employer_description], job_title: params[:job_title], job_description: params[:job_description], year_of_experience: params[:year_of_experience], education_requirement: params[:education_requirement], industry: params[:industry], base_salary: params[:base_salary], employment_type_id: params[:employment_type_id])
+    @new_job = Job.create(url: params[:url], employer_name: params[:employer_name], employer_description: params[:employer_description], job_title: params[:job_title], job_description: params[:job_description], year_of_experience: params[:year_of_experience], education_requirement: params[:education_requirement], industry: params[:industry], base_salary: params[:base_salary], employment_type_id: params[:employment_type_id])
 
-    render json: {job: new_job}
+    render 'jobs/create.jbuilder'
+  end
+
+  #GET /jobs
+  def index
+    @new_job = Job.all
+    render 'jobs/index.jbuilder'
   end
 end
---
+++ app/views/jobs/index.jbuilder
@@ -0,0 +1,12 @@
+json.job @new_job do |j|
+ json.url j.url
+json.employer_name j.employer_name
+json.employer_description j.employer_description
+json.job_title j.job_title
+json.job_description j.job_description
+json.year_of_experience j.year_of_experience
+json.education_requirement j.education_requirement
+json.industry j.industry
+json.base_salary j.base_salary
+json.employment_type_id j.employment_type_id
+end
