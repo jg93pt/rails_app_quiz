c76f615a9ac28feadc8dc2bf6ed1b0632f826118
jg93pt <jorgebenf@gmail.com>
2022-05-13 16:15:24 +0100

    fixed automatic testing for jobs/:id get method

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -49,25 +49,25 @@ RSpec.describe JobsController, type: :controller do
 
   describe 'get /jobs/:id' do
     it 'renders specific job by id in JSON' do
-      job = Job.create(url: 'www.roni.com', employer_name: 'Roni', employer_description: 'Developer', job_title: 'App Developer', job_description: 'Developer of Mobile Apps', year_of_experience: 2, education_requirement: 'Associate Degree', industry: 'IT', base_salary: 1000, employment_type_id: 1997)
+      job_1 = Job.create(url: 'www.johny.com', employer_name: 'Johny', employer_description: 'Developer', job_title: 'App Developer', job_description: 'Developer of Mobile Apps', year_of_experience: 2, education_requirement: 'Associate Degree', industry: 'IT', base_salary: 1000, employment_type_id: 2000)
+      job_2 = Job.create(url: 'www.fan.com', employer_name: 'Fan', employer_description: 'Developer', job_title: 'Web Developer', job_description: 'Developer of Web Apps', year_of_experience: 2, education_requirement: 'Associate Degree', industry: 'IT', base_salary: 1000, employment_type_id: 2003)
 
-      get :show, params: { id: job.id }
+      get :show, params: { id: job_1.id }
 
-      expect(Job.count).to eq(1)
 
       expected_response = {
         job: {
-          id: job.id,
-            url: job.url,
-            employer_name: job.employer_name,
-            employer_description: job.employer_description,
-            job_title: job.job_title,
-            job_description: job.job_description,
-            year_of_experience: job.year_of_experience,
-            education_requirement: job.education_requirement,
-            industry: job.industry,
-            base_salary: job.base_salary,
-            employment_type_id: job.employment_type_id
+          id: job_1.id,
+            url: job_1.url,
+            employer_name: job_1.employer_name,
+            employer_description: job_1.employer_description,
+            job_title: job_1.job_title,
+            job_description: job_1.job_description,
+            year_of_experience: job_1.year_of_experience,
+            education_requirement: job_1.education_requirement,
+            industry: job_1.industry,
+            base_salary: job_1.base_salary,
+            employment_type_id: job_1.employment_type_id
         }
       }
     end
